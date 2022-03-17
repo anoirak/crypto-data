bf8f0893d01c3507e5ab5bf7e497102053ce03f4
anoirak <49559578+anoirak@users.noreply.github.com>
2022-03-17 21:32:46 +0100

    Run PancakeSwap API

--- main_stocks_live.py
+++ main_stocks_live.py
@@ -1,4 +1,11 @@
 import classes.Live.getLive as l
+from classes.Live.getPancake import getPancake
 
 # Get Live Data
 l.getData()
+#Run PancakeSwap API
+tokens = getPancake()
+try:
+    tokens.main()
+except Exception as e:
+    print(e)
